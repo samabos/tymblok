cdfe6f0c2d2311fcd145b6f5a2b0f01b449d26de
Samson Maborukoje <samaborukoje@gmail.com>
2026-03-15 13:50:20 +0000

    fix scopes

--- apps/api/src/Tymblok.Infrastructure/Services/GoogleCalendarSettings.cs
+++ apps/api/src/Tymblok.Infrastructure/Services/GoogleCalendarSettings.cs
@@ -4,5 +4,5 @@ public class GoogleCalendarSettings
 {
     public string ClientId { get; set; } = string.Empty;
     public string ClientSecret { get; set; } = string.Empty;
-    public string IntegrationScopes { get; set; } = "openid email profile https://www.googleapis.com/auth/calendar.readonly";
+    public string IntegrationScopes { get; set; } = "openid email profile https://www.googleapis.com/auth/calendar.readonly https://www.googleapis.com/auth/calendar.events.readonly";
 }
